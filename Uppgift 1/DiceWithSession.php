87028cd526cbd1b4e0d2a880b71c2f3d0ed33694
speelgren <97473666+speelgren@users.noreply.github.com>
2022-03-08 11:05:43 +0100

    Början på session

--- Uppgift 1/DiceWithSession.php
+++ Uppgift 1/DiceWithSession.php
@@ -1,3 +1,10 @@
+<?php
+
+	// Startar session
+	session_start();
+
+?>
+
 <!doctype html>
 <html lang="en" >
 
@@ -12,16 +19,92 @@
 
 		<div>
 			<?php
-
 				//Var uppmärksam på att PHP-tolken används på ett flertal ställen i filen!
 
-				session_start();
-				include 'include/OneDice.php';
-				include 'include/SixDices.php';
-				$disabled = false;
+				session_regenerate_id(true);
+
+				if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+				$disabled = true;
+
+				//Uppgift 1
+				if( isset($_POST["linkNewGame"])) {
+					$linkNewGame = $_POST["linkNewGame"];
+
+					$nbrOfRounds = 0;
+					$sumOfAllRounds
+					$_SESSION["nbrOfRounds"] = $nbrOfRounds;
+					$_SESSION["sumOfAllRounds"] = $sumOfAllRounds;
+
+					$disabled = false;
+
+					echo ("<p>" . "New Game!" . "</p>");
+				}
+
+				//Uppgift 2
+				if( isset($_POST["linkExit"])
+				&& isset($_SESSION["nbrOfRounds"])
+				&& isset($_SESSION["sumOfAllRounds"])) {
+					session_unset();
+					session_destroy();
+				}
+
+				//Uppgift 3
+				if( !isset($_POST["linkExit"])
+				&& !isset($_POST["linkRoll"])
+				&& !isset($_POST["linkNewGame"])
+				&& !isset($_SESSION["nbrOfRounds"])
+				&& !isset($_SESSION["sumOfAllRounds"])) {
+					session_unset();
+					session_destroy();
+				}
+
+				//Uppgift 4
+				if( !isset($_POST["linkExit"])
+				&& !isset($_POST["linkRoll"])
+				&& !isset($_POST["linkNewGame"])
+				&& isset($_SESSION["nbrOfRounds"])
+				&& isset($_SESSION["sumOfAllRounds"])) {
+
+					echo ("<h6>" . "Antal spel: " . $_SESSION["nbrOfRounds"] . "</h6>");
+					echo ("<h6>" . "Summan av alla spel: ". $_SESSION["sumOfAllRounds"] . "</h6>");
+					echo ("<h6>" . "Medel: ". $medel . "</h6>");
+
+				}
+
+				//Uppgift 5
+				if( isset($_POST["linkRoll"]) && isset($_SESSION["nbrOfRounds"]) && isset($_SESSION["sumOfAllRounds"])) {
+
+					include 'include/OneDice.php';
+					include 'include/SixDices.php';
+
+					$obSixDices = new SixDices();
+					$obSixDices->rollDices();
+
+					echo($obSixDices->svgDices());
+
+					$nbrOfRounds = $_SESSION["nbrOfRounds"];
+					$nbrOfRounds++;
+					$_SESSION["nbrOfRounds"] = $nbrOfRounds;
+
+					$sumOfAllRounds = $_SESSION["sumOfAllRounds"];
+					$sumOfAllRounds += $obSixDices->sumDices();
+					$_SESSION["sumOfAllRounds"] = $sumOfAllRounds;
+
+					$medel = $sumOfAllRounds / $nbrOfRounds;
+					echo ("<h6>" . "Antal spel: " . $_SESSION["nbrOfRounds"] . "</h6>");
+					echo ("<h6>" . "Summan av alla spel: ". $_SESSION["sumOfAllRounds"] . "</h6>");
+					echo ("<h6>" . "Medel: ". $medel . "</h6>");
+
+				}
+
+				//Uppgift 6
+				if( !isset($_SESSION["nbrOfRounds"]) && !isset($_SESSION["sumOfAllRounds"])) {
+					disabled = true;
+				}
 
 
-
+}
 			?>
 		</div>
 
